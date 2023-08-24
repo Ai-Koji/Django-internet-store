10ab0a539fb30ba8951b80233b1847072dfce7c4
Lost-S0ul <kireev.ivan.8@gmail.com>
2023-08-24 16:45:34 +0700

    feat: added script for sliders
    if slider have 1 slide, script delete buttons for slider
    
    also if second slider dont have slider script hide button to slider

--- Django/static/scripts/product.js
+++ Django/static/scripts/product.js
@@ -1,3 +1,27 @@
+// hide slider buttons for slider with 1 slide
+arrowPrev = document.querySelectorAll(".arrow-prev")
+arrowNext = document.querySelectorAll(".arrow-next")
+
+// first slider
+sliderCount = document.querySelectorAll("#main-img-slide").length;
+if (sliderCount == 1) {
+    arrowPrev[0].style.display = "none";
+    arrowNext[0].style.display = "none";
+}
+
+// second slider
+sliderCount = document.querySelectorAll(".img-slide").length;
+if (sliderCount == 1) {
+    arrowPrev[1].style.display = "none";
+    arrowNext[1].style.display = "none";
+}
+
+// hide the second slider if it's clean
+if (sliderCount == 0) {
+    document.querySelector("#button-3").style.display = "none";
+}
+
+
 
 // Offer
 function openOffer(){
